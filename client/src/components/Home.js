8763d794d47d6c16039e1a1ab6b3bd84eabee42f
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-11-05 15:34:20 +0100

    use react-chartjs-2

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { memo, useEffect, useState } from "react";
 import { fetchAllTemperatures, fetchCurrentTemperature } from "./../services/api";
+import { Line } from 'react-chartjs-2';
 import Loader from "./../utils/Loader";
 
 import "../styles/Home.css";
@@ -7,13 +8,53 @@ import "../styles/Home.css";
 const Home = () => {
   const [temperatures, setTemperatures] = useState(undefined);
   const [current, updateCurrent] = useState(undefined);
+  const [data, setData] = useState(undefined);
 
   useEffect(() => {
     window.scrollTo(0, 0);
     (async () => {
       const result = await fetchAllTemperatures();
-      console.log(result);
+      console.log(result)
       setTemperatures(result);
+      console.log("toto");
+      console.log({
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: result.data.map(elem => {
+          console.log(elem)
+          return {
+            label: elem.id
+            /*
+            label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+        */
+          }
+        })
+      });
+      setData({
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: result.data.map(elem => {
+          return [
+            
+          ]
+        })
+      })
     })();
   }, []);
 
@@ -36,8 +77,11 @@ const Home = () => {
         )};
       </div>
       <div className="graph">
-        {(temperatures) ? (
+        {(data) ? (
           <>
+            <Line
+              data={data}
+            />
           </>
         ) : (
         <Loader />
