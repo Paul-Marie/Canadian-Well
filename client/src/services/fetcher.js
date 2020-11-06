de1227488bb97d73bbb12a7327c2502f0881a6d5
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-11-06 16:52:23 +0100

    refacto a little bit the fetcher

--- client/src/services/fetcher.js
+++ client/src/services/fetcher.js
@@ -4,14 +4,8 @@ const fetcher = (method) => (
   async (url, payload, type = "application/json;charset=UTF-8") => {
     const result = await fetch(`${serverUrl}${url}`, {
       method,
-      headers: new Headers({
-        "Content-Type": type,
-      }),
-      body: (payload !== undefined)
-        ? type !== "application/json;charset=UTF-8"
-        ? payload
-        : JSON.stringify(payload)
-      : undefined
+      headers: new Headers({ "Content-Type": type }),
+      body: (payload) ? JSON.stringify(payload) : undefined
     });
     if (result.ok === false || result.status === 204)
       return undefined;
